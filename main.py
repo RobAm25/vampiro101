77723ca73081417c3c820026292827d680a7ae01
RobAm25 <roberto.arellanes@gmail.com>
2026-06-04 20:47:51 -0600

    Function sum working

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("This line will be printed.")
+def sum(a, b):
+    result = a + b
+    return result
+
+def main():
+  result = sum(2, 3) 
+  print("Sum is" +  str(result))
+
+if __name__ == "__main__":
+  main()
